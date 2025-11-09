2240610e2cfcf0910fd0ef7d2f9462c76a52395e
arunchauhan02 <100798589+arunchauhan02@users.noreply.github.com>
2025-11-09 09:16:02 +0530

    Update api.py
    Added code for Server

--- api.py
+++ api.py
@@ -1,1 +1,16 @@
 
+from flask import Flask, request, jsonify
+from extractor import ExtractFromResume # import your function
+
+app = Flask(__name__)
+
+@app.route('/extract', methods=['POST'])
+def extract_api():
+    data = request.json
+    text = data.get("text", "")
+
+    result = ExtractFromResume(text)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
