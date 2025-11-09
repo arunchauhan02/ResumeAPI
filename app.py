35c5708e0f5ad01474fbcf8c7da2dfba98878966
arunchauhan02 <100798589+arunchauhan02@users.noreply.github.com>
2025-11-09 12:13:13 +0530

    Update app.py
    Port added

--- app.py
+++ app.py
@@ -20,4 +20,6 @@ def extract_api():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=False) 
+
